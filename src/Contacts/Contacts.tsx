58fe3e290e9c9ac5cd3a4fdeff2c535730b88652
Pavel1706 <80953762+Pavel1706@users.noreply.github.com>
2021-10-26 00:35:08 +0300

    upgrade Contacts.tsx

--- src/Contacts/Contacts.tsx
+++ src/Contacts/Contacts.tsx
@@ -4,23 +4,18 @@ import style from "./Contacts.module.css";
 import styleContainer from "../common/styles/Container.module.css";
 
 
-
-
-
 export function Contacts() {
-
 
 
     return <div className={`${styleContainer.container} ${style.head}`}>
         <div className={style.main}>
-        <div className={style.description}>Contacts</div>
+            <div className={style.description}>Contacts</div>
             <form className={style.form}>
                 <input className={style.input}/>
                 <input className={style.inputSend}/>
                 <textarea className={style.text}/>
+                <button type="submit" className={style.button}>Send</button>
             </form>
-        <div className={style.button}>
-            <a >Send</a></div>
         </div>
     </div>
 
