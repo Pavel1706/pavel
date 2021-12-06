b23087bd0b9d3948ae7089a63db6c75e65807fb7
Pavel1706 <80953762+Pavel1706@users.noreply.github.com>
2021-12-06 13:39:27 +0300

    change BurgerNav.tsx

--- src/common/components/burgerNav/BurgerNav.tsx
+++ src/common/components/burgerNav/BurgerNav.tsx
@@ -1,52 +1,53 @@
-import React from 'react';
+import React, {useState} from 'react';
 import style from './BurgerNav.module.scss';
-import { Link} from "react-scroll";
-
-
+import {Link} from "react-scroll";
 
 
 export function BurgerNav() {
 
+    let [menuIsOpen, setMenuIsOpen ]= useState(false);
+
+
     return (
         <div className={style.burgerNav}>
-            <Link
-                activeClass={style.active}
-                to="main"
-                spy={true}
-                smooth={true}
-                offset={1}
-                duration={500}
-            >Main</Link>
-            {/*<a href="#main">Main</a>*/}
-            {/*<a href="#skills">Skills</a>*/}
-            <Link
-                activeClass={style.active}
-                to="skills"
-                spy={true}
-                smooth={true}
-                offset={1}
-                duration={500}
-            >Skills</Link>
-            {/*<a href="#projects">Projects</a>*/}
-            <Link
-                activeClass={style.active}
-                  to="projects"
-                  spy={true}
-                  smooth={true}
-                  offset={1}
-                  duration={500}
-            >Projects</Link>
-            <Link
-                activeClass={style.active}
-                to="contacts"
-                spy={true}
-                smooth={true}
-                offset={1}
-                duration={500}
-            >Contacts</Link>
-            {/*<a href="#contacts">Contacts</a>*/}
-
-
+            <div className={menuIsOpen ? `${style.burgerNavItems} ${style.show} ` : style.burgerNavItems}>
+                <Link
+                    activeClass={style.active}
+                    to="main"
+                    spy={true}
+                    smooth={true}
+                    offset={1}
+                    duration={500}
+                >Main</Link>
+                {/*<a href="#main">Main</a>*/}
+                {/*<a href="#skills">Skills</a>*/}
+                <Link
+                    activeClass={style.active}
+                    to="skills"
+                    spy={true}
+                    smooth={true}
+                    offset={1}
+                    duration={500}
+                >Skills</Link>
+                {/*<a href="#projects">Projects</a>*/}
+                <Link
+                    activeClass={style.active}
+                    to="projects"
+                    spy={true}
+                    smooth={true}
+                    offset={1}
+                    duration={500}
+                >Projects</Link>
+                <Link
+                    activeClass={style.active}
+                    to="contacts"
+                    spy={true}
+                    smooth={true}
+                    offset={1}
+                    duration={500}
+                >Contacts</Link>
+            </div>
+            <div className={style.burgerButton} onClick={()=>{setMenuIsOpen(!menuIsOpen)}}></div>
         </div>
     );
 }
