cdb59e359ba4416a46f65a557139aef372b9ceb4
Pavel1706 <80953762+Pavel1706@users.noreply.github.com>
2021-12-26 14:05:24 +0300

    upgrade Main.tsx

--- src/main/Main.tsx
+++ src/main/Main.tsx
@@ -1,20 +1,106 @@
 import React from 'react';
-import style from './Main.module.css';
-import styleContainer from '../common/styles/Container.module.css'
+import style from './Main.module.scss';
+import Particles from "react-tsparticles";
+import { Fade } from "react-awesome-reveal";
+import ReactTypingEffect from 'react-typing-effect'
+import Tilt from 'react-parallax-tilt'
+
 
 
 export function Main() {
+
+
+
     return (
-        <div className={style.mainBlock}>
-            <div className={styleContainer.container}>
-                <div className={style.greetings}>
-                    <span>Hello there</span>
-                    <h1> I'm Radzishevskii Pavel</h1>
-                    <p>Frontend-end Developer.</p>
+        <div id='main' className={style.mainBlock}>
+            <Particles className={style.particles}
+                       options={{
+                           // background: {
+                           //     color: {
+                           //         value: "#0d47a1",
+                           //     },
+                           // },
+                           // fpsLimit: 60,
+                           // interactivity: {
+                           //     // events: {
+                           //     //     onClick: {
+                           //     //         enable: true,
+                           //     //         mode: "push",
+                           //     //     },
+                           //     //     onHover: {
+                           //     //         enable: true,
+                           //     //         mode: "repulse",
+                           //     //     },
+                           //     //     resize: true,
+                           //     // },
+                           //     modes: {
+                           //         // bubble: {
+                           //         //     distance: 400,
+                           //         //     duration: 2,
+                           //         //     opacity: 0.8,
+                           //         //     size: 40,
+                           //         // },
+                           //         // push: {
+                           //         //     quantity: 4,
+                           //         // },
+                           //         // repulse: {
+                           //         //     distance: 200,
+                           //         //     duration: 0.4,
+                           //         // },
+                           //     },
+                           // },
+                           particles: {
+                               color: {
+                                   value: "#ffffff",
+                               },
+                               links: {
+                                   color: "#ffffff",
+                                   distance: 150,
+                                   enable: true,
+                                   opacity: 0.5,
+                                   width: 1,
+                               },
+                               // collisions: {
+                               //     enable: true,
+                               // },
+                               move: {
+                                   // direction: "none",
+                                   enable: true,
+                                   outMode: "bounce",
+                                   random: false,
+                                   speed: 1,
+                                   straight: false,
+                               },
+
+                               opacity: {
+                                   value: 0.5,
+                               },
+                               shape: {
+                                   type: "circle",
+                               },
+                           },
+                       }}/>
+            <Fade delay={1000}>
+            <div className={style.container}>
+                <div className={style.greeting}>
+                    <span >Hello there</span>
+                    <span>I am Pavel <span>Radzishevskii</span></span>
+                    <ReactTypingEffect
+                        text="Frontend Developer." speed={100}
+                    />
                 </div>
-                <div className={style.photo}>
-                </div>
+                <Tilt>
+                    <div>
+                        <div className={style.photo}>
+                            <div className={style.image}>
+
+                            </div>
+                        </div>
+                    </div>
+                </Tilt>
+
             </div>
+            </Fade>
         </div>
     );
 }
