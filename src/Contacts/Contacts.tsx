098cf098ebedc953a238859035f3988d8ff6b407
Pavel1706 <80953762+Pavel1706@users.noreply.github.com>
2021-12-26 14:03:06 +0300

    upgrade Contacts.tsx

--- src/Contacts/Contacts.tsx
+++ src/Contacts/Contacts.tsx
@@ -1,21 +1,24 @@
 import React from 'react';
-
-import style from "./Contacts.module.css";
-import styleContainer from "../common/styles/Container.module.css";
+import style from "./Contacts.module.scss";
+import {Title} from "../common/components/title/Title";
+import {Fade} from "react-awesome-reveal";
 
 
 export function Contacts() {
-
-
-    return <div className={`${styleContainer.container} ${style.head}`}>
-        <div className={style.main}>
-            <div className={style.description}>Contacts</div>
+    // return <div className={`${styleContainer.container} ${style.head}`}>
+    return <div id='contacts' className={style.contactsBlock}>
+        <div className={style.container}>
+            <Title text={'Contacts'}/>
+            <Fade delay={1000}>
             <form className={style.form}>
-                <input className={style.input}/>
-                <input className={style.inputSend}/>
-                <textarea className={style.text}/>
-                <button type="submit" className={style.button}>Send</button>
+                <input type="text" className={style.formArea} placeholder='Name'/>
+                <input type="text" className={style.formArea} placeholder='e-mail'/>
+                <textarea className={style.messageArea} placeholder='Message'/>
+                {/*<button type="submit" className={style.button}>Send</button>*/}
+                {/*<Button  text={'Send'}/>*/}
+                <button type="submit" >Send</button>
             </form>
+            </Fade>
         </div>
     </div>
 
