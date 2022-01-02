bec3aac91bcdaa172cb85839c9acfccfe44cf32c
Pavel1706 <80953762+Pavel1706@users.noreply.github.com>
2022-01-02 22:03:32 +0300

    change Project.tsx

--- src/Projects/Project/Project.tsx
+++ src/Projects/Project/Project.tsx
@@ -1,12 +1,13 @@
 import React from 'react';
-import style from './Project.module.css';
+import style from './Project.module.scss';
 
 type ProjectType ={
     id:string
     photo: string
     name: string
     description: string
-    background:   string
+    background:  string
+
 }
 type AllProjectType={
     projectDescribe: Array<ProjectType>
@@ -16,11 +17,27 @@ type AllProjectType={
 export function Project(props:AllProjectType) {
 
 
+
+
+    const seeProject=(projectId:string)=>{
+
+            if(props.projectDescribe[0].id===projectId) {
+                debugger
+                window.location.href = 'http://Pavel1706.github.io/SocialNetWork'
+            }
+            if(props.projectDescribe[1].id===projectId){
+                window.location.href='http://Pavel1706.github.io/appTodo'
+            }
+
+    }
+
+
     return (<>
     {props.projectDescribe.map(t=>
         <div key={t.id} className={style.project}>
             <div className={style.icon} style={{backgroundImage:t.background}}>
-            <a className={style.button}>push</a></div>
+               <button type="submit" onClick={()=>{seeProject(t.id)}}>Push me</button> </div>
+
             <div className={style.secondMain}>
             <h3 className={style.projectTitle}>{t.name}</h3>
         <div className={style.description}>{t.description}</div>
