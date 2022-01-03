a02d0ef099a22e663c5ca0f67c6d85feeffcfeea
Pavel1706 <80953762+Pavel1706@users.noreply.github.com>
2022-01-03 16:43:05 +0300

    change RemoteWork.tsx

--- src/RemoteWork/RemoteWork.tsx
+++ src/RemoteWork/RemoteWork.tsx
@@ -1,21 +1,20 @@
 import React from 'react';
 
-import style from "./RemoteWork.module.css";
+import style from "./RemoteWork.module.scss";
 import styleContainer from "../common/styles/Container.module.css";
+import {Title} from "../common/components/title/Title";
+import {Button} from "../common/components/button/Button";
 
 
 
 
 
 export function RemoteWork() {
-
-
-
     return <div className={`${styleContainer.container} ${style.head}`}>
         <div className={style.main}>
-        <div className={style.description}>You wont`t believe it, I can remote work</div>
-        <div className={style.button}>
-            <a >Call me</a></div>
+        <Title text={`I can perform work remotely`}/>
+            <button type="submit" >Call me</button>
+            {/*<Button text={'Call me'}/>*/}
         </div>
     </div>
 
