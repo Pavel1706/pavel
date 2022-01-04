caa01c7ff5250f3eb91e02dfa3c17af58491dc1c
Pavel1706 <80953762+Pavel1706@users.noreply.github.com>
2022-01-04 12:25:55 +0300

    change Skill.tsx

--- src/skills/skill/Skill.tsx
+++ src/skills/skill/Skill.tsx
@@ -1,10 +1,11 @@
 import React from 'react';
-import style from './Skill.module.css';
+import style from './Skill.module.scss';
 
 type SkillType ={
     id:string
     title:string
     text: string
+    background: string
 }
 type AllSkillType={
     skillDescribe: Array<SkillType>
@@ -16,7 +17,8 @@ export function Skill(props:AllSkillType) {
 
     return<div className={style.main}>{props.skillDescribe.map(t=>
         <div key={t.id} className={style.skill}>
-        <div className={style.icon}>0</div>
+
+        <div className={style.icon} style={{backgroundImage:t.background}}></div>
                 <h3>{t.title}</h3>
                 <span className={style.description}>{t.text}</span>
 
